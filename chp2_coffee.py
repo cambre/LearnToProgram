5edb96d4650ed17cdcc0fdfa63f4ff0d259daff4
cambre <bigslick428@gmail.com>
2014-08-25 22:25:43 -0400

    # Loyal customer Price # Only displays when price is under $4.75

--- chp2_coffee.py
+++ chp2_coffee.py
@@ -1,13 +1,15 @@
+# Loyal customer Price
+# Only displays when price is under $4.75
+
 __author__ = 'Cambre'
 
 import urllib.request
-page = urllib.request.urlopen("http://beans-r-us.appspot.com/prices-loyalty.html")
-text = page.read().decode("utf-8")
-
-where = text.find('>$')
-
-start_of_price = where + 2
-end_of_price = start_of_price + 4
-
-price = text[start_of_price:end_of_price]
+price = 99.99
+while price > 4.75:
+    page = urllib.request.urlopen("http://beans-r-us.appspot.com/prices-loyalty.html")
+    text = page.read().decode("utf-8")
+    where = text.find('>$')
+    start_of_price = where + 2
+    end_of_price = start_of_price + 4
+    price = float(text[start_of_price:end_of_price])
 print(price)
