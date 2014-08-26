7f911b1498053e62fb5ef931962c9485e06164bf
cambre <bigslick428@gmail.com>
2014-08-25 21:51:11 -0400

    Random number now. Runs on a loop until number guessed correctly.

--- chp1_guessNumber.py
+++ chp1_guessNumber.py
@@ -1,13 +1,19 @@
 # Chapter 1
+# Includes random number from 1-10.
+# Runs on loop until guess is correct.
 __author__ = 'Cambre'
+from random import randint
+secret = randint(1,10)
 print("Welcome")
-g = input("Guess the number: ")
-guess = int(g)
-if guess ==5:
-    print ("You win!")
-else:
-    if guess < 5:
-        print ("Too low!")
+guess = 0
+while guess != secret:
+    g = input("Guess the number from 1 to 10: ")
+    guess = int(g)
+    if guess == secret:
+        print ("You win!!!")
     else:
-        print ("Too high!")
+        if guess > secret:
+            print ("Too high")
+        else:
+            print ("Too low!")
 print ("Game over!")
