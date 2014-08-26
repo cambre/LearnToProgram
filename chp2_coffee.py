f0aece51fbbcf2e54a987bde1a21bed237ad2b71
cambre <bigslick428@gmail.com>
2014-08-25 22:15:24 -0400

    Loyal customer price

--- chp2_coffee.py
+++ chp2_coffee.py
@@ -1,7 +1,13 @@
 __author__ = 'Cambre'
 
 import urllib.request
-page = urllib.request.urlopen("http://beans-r-us.appspot.com/prices.html")
-text = page.read().decode("utf8")
+page = urllib.request.urlopen("http://beans-r-us.appspot.com/prices-loyalty.html")
+text = page.read().decode("utf-8")
 
-print(text)
+where = text.find('>$')
+
+start_of_price = where + 2
+end_of_price = start_of_price + 4
+
+price = text[start_of_price:end_of_price]
+print(price)
