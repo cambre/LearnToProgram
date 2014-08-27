d23ced0b423db43f313713aa8b87bdc049cbe5ca
cambre <bigslick428@gmail.com>
2014-08-26 23:50:32 -0400

    Sends tweet that says "Buy!" if price is below a certain point.  Or it sends you the price when you answer yes.
    You can use pip to install twython.  Found this as the easiest solution after twitter changed their API rules and you couldn't follow the instructions in the book anymore.

--- chp3_coffee.py
+++ chp3_coffee.py
@@ -1,24 +1,37 @@
-# Page 90
-__author__ = 'Cambre'
+# Send_to_twitter_status
+# Had to be modified - can't follow instructions in book for Twitter
 
 import urllib.request
 import time
+import twython
+
+def send_to_twitter(msg):
+    
+    CONSUMER_KEY = ''
+    CONSUMER_SECRET = ''
+    ACCESS_KEY = ''
+    ACCESS_SECRET = ''
+    
+    twitter = twython.Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+    twitter.update_status(status=msg)    
 
 def get_price():
+
     page = urllib.request.urlopen("http://beans-r-us.appspot.com/prices-loyalty.html")
-    text = page.read().decode("utf-8")
+    text = page.read().decode ("utf8")
     where = text.find('>$')
     start_of_price = where + 2
     end_of_price = start_of_price + 4
-    print("The price of coffee is:")
-    return(float(text[start_of_price:end_of_price]))
+    return float(text[start_of_price:end_of_price])
 
-price_now = input("Do you want to see the price now (Y/N)?")
+price_now = input("Do you want to see the price now (Y/N)? ")
+
 if price_now == "Y":
-    print(get_price())
+    send_to_twitter(get_price())
+
 else:
     price = 99.99
     while price > 4.74:
-        time.sleep(15) #900 for 15 minutes
+        time.sleep(10)
         price = get_price()
-    print(price)
+    send_to_twitter("Buy!")
