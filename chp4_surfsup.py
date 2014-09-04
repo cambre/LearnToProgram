e9ab7808866a7d9b20b3485ee8f4902e8ab0cb9c
cambre <bigslick428@gmail.com>
2014-09-03 21:51:42 -0400

    Added array and sorted the results so I could display high to low.

--- chp4_surfsup.py
+++ chp4_surfsup.py
@@ -1,11 +1,14 @@
 __author__ = 'Cambre'
-
+scores =[]
 highest_score = 0
 results_f = open("results.txt")
 for line in results_f:
     (name, score) = line.split()
-    if float(score) > highest_score:
-        highest_score = float(score)
+    scores.append(float(score))
 results_f.close()
-print("The highest score was:")
-print(highest_score)
+scores.sort()
+scores.reverse()
+print("The top scores were:")
+print(scores[0])
+print(scores[1])
+print(scores[2])
