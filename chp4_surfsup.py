66a83b8a8da503bc0aaddb1b6b702088334ddb96
cambre <bigslick428@gmail.com>
2014-09-03 22:01:28 -0400

    highest_score variable no longer needed.  REMOVED Added second array to display names.  Doesn't work as names and scores are now seperate lists, and don't line up.  Do not use.  Update coming soon.

--- chp4_surfsup.py
+++ chp4_surfsup.py
@@ -1,14 +1,17 @@
 __author__ = 'Cambre'
 scores =[]
-highest_score = 0
+names =[]
 results_f = open("results.txt")
 for line in results_f:
     (name, score) = line.split()
     scores.append(float(score))
+    names.append(name)
 results_f.close()
 scores.sort()
 scores.reverse()
+names.sort()
+names.reverse()
 print("The top scores were:")
-print(scores[0])
-print(scores[1])
-print(scores[2])
+print(names[0] + ' with ' + str(scores[0]))
+print(names[1] + ' with ' + str(scores[1]))
+print(names[2] + ' with ' + str(scores[2]))
